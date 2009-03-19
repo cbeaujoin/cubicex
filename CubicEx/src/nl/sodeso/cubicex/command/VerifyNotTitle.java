6c87e042ba2fdd4006e5a95f6e79a947145f5afe
Ronald Mathies <ronald@sodeso.nl>
2009-03-19 22:50:01 +0000

    New commands

---
+++ CubicEx/src/nl/sodeso/cubicex/command/VerifyNotTitle.java
@@ -0,0 +1,54 @@
+/**
+ *  Copyright 2009 Sodeso 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+package nl.sodeso.cubicex.command;
+
+import java.util.Map;
+
+import nl.sodeso.cubicex.CubicExBaseTestCase;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.cubictest.selenium.custom.IElementContext;
+
+import com.thoughtworks.selenium.Selenium;
+
+/**
+ * Replaces the <code>verifyTitle</code> command of SeleniumIDE
+ * 
+ * @author r.mathies
+ * @version 0.0.2
+ */
+public class VerifyNotTitle extends CubicExBaseTestCase {
+
+	private Log log = LogFactory.getLog(VerifyNotTitle.class);
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public void executeTest(final Map<String, String> arguments, final IElementContext context, final Selenium selenium) throws Exception {
+		// Retrieve the parameters.
+		final String _valueToCompareTo = getArgValue();
+
+		if (log.isInfoEnabled()) {
+			log.info("VerifyTitle: value '" + _valueToCompareTo + "'.");
+		}
+		
+		// Retrieve the field value.
+		String _valueOfTitle = selenium.getTitle();
+		verifyNotEquals(_valueOfTitle, _valueToCompareTo);
+	}
+
+}
